a3c564463f759c2a396eba1272375f2e749c0530
Rui Santos <rui.pmfs@gmail.com>
2021-10-05 23:01:21 +0100

    Initial Structure

--- ggc-core/src/ggc/WarehouseManager.java
+++ ggc-core/src/ggc/WarehouseManager.java
@@ -14,6 +14,46 @@ public class WarehouseManager {
 		// TO DO
 	}
 
+	public void save() {
+		// TO DO
+	}
+
+	public int getCurrentDate() {
+		// TO DO
+	}
+
+	public void advanceDays(int daysToAdvance) {
+		// TO DO
+	}
+
+	public int checkCurrentBalance() {
+		// TO DO
+	}
+
+	public void checkAllProducts() {
+		// TO DO
+	}
+
+	public void checkAllBatches() {
+		// TO DO
+	}
+
+	public void checkBatchesByPartner(String partnerKey) {
+		// TO DO
+	}
+
+	public void checkBatchesByProduct(String productKey) {
+		// TO DO
+	}
+
+	public void showPartnerInfo(String partnerKey) {
+		// TO DO
+	}
+
+	public void checkAllBatches() {
+		// TO DO
+	}
+
 	public float getAvailableBalance() {
 		// TO DO
 	}
